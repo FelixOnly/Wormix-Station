a28393e6a60fea485eba71ef11e30c712b1fbcbf
Felix <62942680+FelixOnly@users.noreply.github.com>
2026-07-10 22:57:57 +0300

    [Fix] Directional emotes on by default

--- Content.Shared/_Orion/CCVar/CCVars.Chat.cs
+++ Content.Shared/_Orion/CCVar/CCVars.Chat.cs
@@ -12,7 +12,7 @@ public sealed partial class CCVars
     ///     Enable or disable directional emotes systems.
     /// </summary>
     public static readonly CVarDef<bool> DirectionalEmotesEnabled =
-        CVarDef.Create("chat.directional_emotes_enabled", false, CVar.REPLICATED);
+        CVarDef.Create("chat.directional_emotes_enabled", true, CVar.REPLICATED);
 
     /// <summary>
     ///     The range at which directional emotes can be used.
